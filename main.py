ec2d7e2d76911d3a0a235c3a8e160c1fd615fc7f
김희주 <heejukim1873@c5r9s1.codyssey.kr>
2026-08-04 12:13:11 +0900

    Feat: 메뉴 기능 구현

--- main.py
+++ main.py
@@ -1,37 +1,5 @@
-from quiz import Quiz
+from quiz_game import QuizGame
 
-quiz1 = Quiz(
-    "판다가 주로 먹는 음식은 무엇일까요?",
-    ["사과", "대나무", "고기", "옥수수"],
-    2
-)
+game = QuizGame()
 
-quiz2 = Quiz(
-    "바다에서 가장 큰 동물은 무엇일까요?",
-    ["상어", "돌고래", "대왕오징어", "대왕고래"],
-    4
-)
-
-quiz3 = Quiz(
-    "캥거루가 새끼를 키우는 곳은 어디일까요?",
-    ["둥지", "굴", "주머니", "나무 위"],
-    3
-)
-
-quiz4 = Quiz(
-    "박쥐는 어떤 동물일까요?",
-    ["새", "곤충", "포유류", "파충류"],
-    3
-)
-
-quiz5 = Quiz(
-    "코알라가 주로 먹는 것은 무엇일까요?",
-    ["대나무", "유칼립투스 잎", "바나나", "도토리"],
-    2
-)
-
-quizzes = [quiz1, quiz2, quiz3, quiz4, quiz5]
-
-for quiz in quizzes:
-    quiz.display()
-    print()
+game.run()
--- quiz_game.py
+++ quiz_game.py
@@ -1,2 +1,36 @@
 class QuizGame:
-    pass
+
+    def show_menu(self):
+        print("\n===== 🐾 동물 퀴즈 게임 =====")
+        print("1. 퀴즈 풀기")
+        print("2. 퀴즈 추가")
+        print("3. 퀴즈 목록")
+        print("4. 최고 점수")
+        print("5. 종료")
+
+    def run(self):
+
+        while True:
+
+            self.show_menu()
+
+            menu = input("메뉴를 선택하세요: ")
+
+            if menu == "1":
+                print("퀴즈 풀기 기능")
+
+            elif menu == "2":
+                print("퀴즈 추가 기능")
+
+            elif menu == "3":
+                print("퀴즈 목록 기능")
+
+            elif menu == "4":
+                print("최고 점수 기능")
+
+            elif menu == "5":
+                print("프로그램을 종료합니다.")
+                break
+
+            else:
+                print("잘못된 입력입니다.")
